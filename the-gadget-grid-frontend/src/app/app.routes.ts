50a866108eefbd27bd8261cf7d5f531807298fd1
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-25 04:14:10 +0530

    Add HTTP client provider and auth routes

--- the-gadget-grid-frontend/src/app/app.routes.ts
+++ the-gadget-grid-frontend/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { Home } from './page/home/home';
+import { Logincomponent } from './component/logincomponent/logincomponent';
+import { Createaccountcomponent } from './component/createaccountcomponent/createaccountcomponent';
 
 export const routes: Routes = [
     {
         path: '',
         component:Home
+    },
+    {
+        path: 'login',
+        component: Logincomponent
+    },
+    {
+        path:'create-account',
+        component: Createaccountcomponent
     }
 ];
