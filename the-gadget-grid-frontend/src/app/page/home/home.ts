5daf98b0bbc1b5ae1fcead2122f3bca8dd4d261a
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-25 04:16:03 +0530

    Add Product component to Home page
    Render the product component on the home page by inserting <app-productcomponent> into home.html and importing Productcomponent in home.ts. Also add Productcomponent to the component's imports array so Angular will recognize and compile it.

--- the-gadget-grid-frontend/src/app/page/home/home.ts
+++ the-gadget-grid-frontend/src/app/page/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from "../../component/navbar/navbar";
 import { Footer } from "../../component/footer/footer";
 import { Homecomponent } from "../../component/homecomponent/homecomponent";
+import { Productcomponent } from "../../component/productcomponent/productcomponent";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Footer, Homecomponent],
+  imports: [Navbar, Footer, Homecomponent, Productcomponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
