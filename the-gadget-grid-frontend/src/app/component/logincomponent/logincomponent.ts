ed3575b6a9bd136301791a7c35ee7dcc7b079221
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-25 04:15:00 +0530

    Add login component with template and tests

---
+++ the-gadget-grid-frontend/src/app/component/logincomponent/logincomponent.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../service.user/userdata';
+
+@Component({
+  selector: 'app-logincomponent',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './logincomponent.html',
+  styleUrl: './logincomponent.css',
+})
+export class Logincomponent {
+  loginData = {
+    username: '',
+    password: ''
+  };
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin(form: NgForm) {
+    this.authService.getAllUsers().subscribe({
+      next: (users: any[]) => {
+        console.log('Database eke inna Usersla:', users);
+
+        const validUser = users.find(u => 
+          u.username === this.loginData.username && 
+          u.password === this.loginData.password
+        );
+
+        if (validUser) {
+          console.log('Success! User match una:', validUser);
+          
+          this.clearForm(form);
+          
+          alert('Login Successful!');
+          this.router.navigate(['/']); 
+        } else {
+          console.error('Login Failed: Username ho Password incorrect');
+          alert('Username or Password incorrect. Login failed!');
+          
+          this.clearForm(form);
+        }
+      },
+      error: (err) => {
+        console.error('Backend eken data ganna bari una:', err);
+        alert('Backend error! Check your connection.');
+      }
+    });
+  }
+
+  clearForm(form: NgForm) {
+    this.loginData = {
+      username: '',
+      password: ''
+    };
+    form.resetForm();
+  }
+}
