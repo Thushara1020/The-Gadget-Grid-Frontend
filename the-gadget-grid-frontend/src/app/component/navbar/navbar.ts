c565ba3b55cd655b3191fc7d4874a5a86668df65
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-28 04:17:54 +0530

    feat(navbar): integrate cart service and user authentication

--- the-gadget-grid-frontend/src/app/component/navbar/navbar.ts
+++ the-gadget-grid-frontend/src/app/component/navbar/navbar.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { Router, RouterModule } from '@angular/router';
+import { Productservice } from '../../service.product/productservice'; 
 
 @Component({
   selector: 'app-navbar',
@@ -11,11 +12,19 @@ import { Router, RouterModule } from '@angular/router';
 })
 export class Navbar implements OnInit {
   userName: string | null = '';
+  cartItemCount: number = 0; 
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private productService: Productservice 
+  ) {}
 
   ngOnInit() {
     this.userName = localStorage.getItem('userName');
+    
+    this.productService.cartItemCount$.subscribe(count => {
+      this.cartItemCount = count;
+    });
   }
 
   isLoggedIn(): boolean {
