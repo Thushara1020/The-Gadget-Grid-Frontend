64653a880f40ce79341a3bc45adf4d521a421807
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-28 04:18:28 +0530

    feat(service): implement persistent cart management in Productservice

--- the-gadget-grid-frontend/src/app/service.product/productservice.ts
+++ the-gadget-grid-frontend/src/app/service.product/productservice.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 export interface Product {
   productId: number;
@@ -21,7 +21,30 @@ export class Productservice {
   
   cartDataList: any[] = []; 
 
-  constructor(private http: HttpClient) {}
+  private cartItemCountSubject = new BehaviorSubject<number>(0);
+  cartItemCount$ = this.cartItemCountSubject.asObservable(); 
+
+  constructor(private http: HttpClient) {
+    this.loadCartFromLocalStorage();
+  }
+
+  private updateCartCount() {
+
+    const totalItems = this.cartDataList.reduce((sum, item) => sum + (item.quantity || 1), 0);
+    this.cartItemCountSubject.next(totalItems);
+    
+
+    localStorage.setItem('cartItems', JSON.stringify(this.cartDataList));
+  }
+
+
+  private loadCartFromLocalStorage() {
+    const savedCart = localStorage.getItem('cartItems');
+    if (savedCart) {
+      this.cartDataList = JSON.parse(savedCart);
+      this.updateCartCount();
+    }
+  }
 
 
   getAllProducts(): Observable<Product[]> {
@@ -33,26 +56,35 @@ export class Productservice {
   }
 
   addToCart(cartData: any): Observable<any> {
+    this.cartDataList.push(cartData);
+    this.updateCartCount();
+
     return this.http.post('http://localhost:8080/api/cart/add', cartData);
   }
 
   addBulkToCart(cartItems: any[]): Observable<any> {
+    this.cartDataList.push(...cartItems);
+    this.updateCartCount();
+
     return this.http.post('http://localhost:8080/api/cart/add-bulk', cartItems);
   }
 
 
   updateCart(data: any[]) {
     this.cartDataList = data;
+    this.updateCartCount();
   }
 
   getCartItems() {
     return this.cartDataList;
   }
+  
   placeOrder(orderData: any): Observable<any> {
-  return this.http.post('http://localhost:8080/api/orders/place', orderData);
-}
-// Productservice.ts එකේ
-getOrdersByUserId(userId: number): Observable<any[]> {
-  return this.http.get<any[]>(`http://localhost:8080/api/orders/user/${userId}`);
-}
+    this.updateCart([]); 
+    return this.http.post('http://localhost:8080/api/orders/place', orderData);
+  }
+
+  getOrdersByUserId(userId: number): Observable<any[]> {
+    return this.http.get<any[]>(`http://localhost:8080/api/orders/user/${userId}`);
+  }
 }
